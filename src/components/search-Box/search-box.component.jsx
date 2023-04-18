223117b77980762ab160a67005d51f13ac5f3d17
Life90 <68822929+Life90@users.noreply.github.com>
2023-04-17 20:40:55 -0700

    new monster class project

---
+++ src/components/search-Box/search-box.component.jsx
@@ -0,0 +1,14 @@
+// const SearchBox = ({ placeholder, onSearchChangeHandler, className }) => {
+//   return (
+//     <div>
+//       <input
+//         className={className}
+//         type="search"
+//         placeholder={placeholder}
+//         onChange={onSearchChangeHandler}
+//       />
+//     </div>
+//   );
+// };
+
+// export default SearchBox;
